3430cbd10ebcccf19d8df9e627007d863caf3db3
jorge <jorge.floresl@mail.udp.cl>
2018-03-26 22:06:32 -0300

    Moahora puedo acceder a base de datos

--- creditos/views.py
+++ creditos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Banco
+def post_list(request):
+ banco=Banco.objects.filter().order_by('cae')
+ return render(request, 'creditos/index.html', {'banco':banco})
 
-# Create your views here.
