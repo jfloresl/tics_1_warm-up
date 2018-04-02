00a9e2ae837a3db0d8e00c23060269153b656069
jorge <jorge.floresl@mail.udp.cl>
2018-04-01 22:01:00 -0300

    ultimos detalle formulario

--- creditos/models.py
+++ creditos/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from .forms import Regform
 class Banco(models.Model):
        cae =models.DecimalField(max_digits=4,decimal_places=2)
+       #interes mensuales
        im_1=models.DecimalField(max_digits=4,decimal_places=2)
        im_2=models.DecimalField(max_digits=4,decimal_places=2)
        im_3=models.DecimalField(max_digits=4,decimal_places=2)
        im_4=models.DecimalField(max_digits=4,decimal_places=2)
+       #seguros
        seguro_1 =models.IntegerField()
        seguro_2 =models.IntegerField()
        seguro_3 =models.IntegerField()
@@ -14,8 +16,6 @@ class Banco(models.Model):
        nombre = models.CharField(max_length=200)
 
 
-       def segu(a):
-           return 666
-
-       def compra(meses,seg,gastos,imp,monto):
+       def ctc(meses,seg,gastos,imp,monto):
+             #formula de valor de cuota * meses=ctc
            return (((monto*((1+(imp/100))**meses)*(imp/100)) / (((1+(imp/100))**meses)-1)) +(seg/meses)+(gastos/meses))*meses
--- creditos/views.py
+++ creditos/views.py
@@ -4,48 +4,55 @@ from .models import *
 
 
 def post_list(request):
+ #hace correr el formulario de forms.py
  form=Regform(request.POST)
  return render(request, 'creditos/index.html',{"form":form})
 
 def listabancos(request):
+ #valores del entregados por el formulario
  monto=int(request.POST.get('monto'))
  meses=int(request.POST.get('meses'))
  seguro=request.POST.get('seguro')
+ #lista de gastos
  gastos=[]
  for i in range(1,11):
-  gastos.append(Banco.objects.values_list('gastos',flat=True).get(pk=i))
+  gastos.append(Banco.objects.values_list('gastos',flat=True).order_by('cae').get(pk=i))
  seg=[]
  imp=[]
+ #selecciona impuesto y seguro segun la cantidad de meses
  if meses>0 and meses<=12:
                 for i in range(1,11):
-                   imp.append(Banco.objects.values_list('im_1',flat=True).get(pk=i))
+                   imp.append(Banco.objects.values_list('im_1',flat=True).order_by('cae').get(pk=i))
                 if seguro=="on":
                  for i in range(1,11):
-                   seg.append(Banco.objects.values_list('seguro_1',flat=True).get(pk=i))
-                banco=Banco.objects.filter().order_by('im_1')
+                   seg.append(Banco.objects.values_list('seguro_1',flat=True).order_by('cae').get(pk=i))
+                banco=Banco.objects.filter().order_by('cae')
  elif meses>=13 and meses<=23:
                 for i in range(1,11):
-                   imp.append(Banco.objects.values_list('im_2',flat=True).get(pk=i))
+                   imp.append(Banco.objects.values_list('im_2',flat=True).order_by('cae').get(pk=i))
                 if seguro=="on":
                  for i in range(1,11):
-                   seg.append(Banco.objects.values_list('seguro_2',flat=True).get(pk=i))
-                banco=Banco.objects.filter().order_by('im_2')
+                   seg.append(Banco.objects.values_list('seguro_2',flat=True).order_by('cae').get(pk=i))
+                banco=Banco.objects.filter().order_by('cae')
  elif meses>=24 and meses<=36:
                 for i in range(1,11):
-                   imp.append(Banco.objects.values_list('im_3',flat=True).get(pk=i))
+                   imp.append(Banco.objects.values_list('im_3',flat=True).order_by('cae').get(pk=i))
                 if seguro=="on":
                  for i in range(1,11):
-                   seg.append(Banco.objects.values_list('seguro_3',flat=True).get(pk=i))
-                banco=Banco.objects.filter().order_by('im_3')
+                   seg.append(Banco.objects.values_list('seguro_3',flat=True).order_by('cae').get(pk=i))
+                banco=Banco.objects.filter().order_by('cae')
  elif meses>=37 and meses<=48:
                  for i in range(1,11):
-                   imp.append(Banco.objects.values_list('im_4',flat=True).get(pk=i))
+                   imp.append(Banco.objects.values_list('im_4',flat=True).order_by('cae').get(pk=i))
                  if seguro=="on":
                   for i in range(1,11):
-                   seg.append(Banco.objects.values_list('seguro_4',flat=True).get(pk=i))
-                 banco=Banco.objects.filter().order_by('im_4')
+                   seg.append(Banco.objects.values_list('seguro_4',flat=True).order_by('cae').get(pk=i))
+                 banco=Banco.objects.filter().order_by('cae')
  elif meses<0 or meses>48:
                 imp=-1
+ #llamado a la funcion para calcular el ctc
+ costo_total=[]
+ for i in range(1,10):
+  costo_total.append(Banco.ctc(float(meses),float(seg[i]),float(gastos[i]),float(imp[i]),monto))
+ return render(request, 'creditos/index2.html',{'banco':banco,'costo_total':costo_total}) 
 
- avedul=Banco.compra(float(meses),float(seg[1]),float(gastos[1]),float(imp[1]),monto)
- return render(request, 'creditos/index2.html',{'banco':banco,'seg':seg,'imp':imp,'avedul':avedul})
